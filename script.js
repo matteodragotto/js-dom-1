85e9f2f34bddfc367f7086fa9bc1c81f5e72d7d2
matteodragotto <matteodrag8@gmail.com>
2024-11-21 18:43:58 +0100

    [feat] bonus

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const interruttore = document.getElementById('light-switch');
 const luce = document.querySelector('.lampada-accesa');
-const buio = document.querySelector('.lampada-spenta')
+const buio = document.querySelector('.lampada-spenta');
+const suonoAccensione = document.getElementById('light-sound');
 
-interruttore.addEventListener('click', () => {
+interruttore.addEventListener('click', () => {  
   luce.classList.toggle('hide')
   buio.classList.toggle('hide')
+
+  if (!luce.classList.contains('hide')) {
+    luce.classList.add('flickering');
+    suonoAccensione.play();
+  }
+
+  setTimeout(() => {
+    luce.classList.remove('flickering');
+    luce.style.opacity = '1';
+  }, 2000);
+
+  interruttore.innerHTML = luce.classList.contains('hide') ? 'Clicca qui per accendere la luce' : 'Clicca qui per spegnere la luce'
     
 })
